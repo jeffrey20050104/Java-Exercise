e4f59545ec8e46019c703e8a862c683e9cbcf3e1
Yanagi0912 <930912jeffery@gmail.com>
2025-04-24 15:58:17 +0800

    01257012游承諺練習GitHub協作

--- FirstClass4Team.java
+++ FirstClass4Team.java
@@ -1,6 +1,7 @@
 public class FirstClass4Team {
     public static void main(String[] args) {
         System.out.println("這是用來練習GitHub協作的程式!");
-        System.out.println("\"學號：\"01257006\"\n姓名：\"張羽傑\"");        
+        System.out.println("\"學號：\"01257006\"\n姓名：\"張羽傑\"");       
+        System.out.println("\"學號：\"01257012\"\n姓名：\"游承諺\""); 
     }
 }
