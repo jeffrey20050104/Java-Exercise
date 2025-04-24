04edce57276a9ad9d954480c4c0e97267eeaac1d
jeffrey <jeffreychang20050104@gmail.com>
2025-04-24 15:48:47 +0800

    create firstclass4team
    create this practice class and add my own name張羽傑--member1

---
+++ FirstClass4Team.java
@@ -0,0 +1,6 @@
+public class FirstClass4Team {
+    public static void main(String[] args) {
+        System.out.println("這是用來練習GitHub協作的程式!");
+        System.out.println("\"學號：\"01257006\"\n姓名：\"張羽傑\"");        
+    }
+}
